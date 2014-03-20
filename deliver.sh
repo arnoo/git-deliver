5975cc89ca67f14bb0509aa07506946f24e636c1
Arnaud Betremieux <arno@arnoo.net>
2014-03-20 08:11:27 +0100

    Portability : Remove -a option in basename call (not available on msys)

--- deliver.sh
+++ deliver.sh
@@ -670,7 +670,7 @@ function get_branch_for_version
 	local eligible_branches=`git branch --contains $1 | tr -d '^ *' | tr -d '^ '`
 
 	# "real" branches, in .git/refs/heads
-	local real_branches=`echo "$eligible_branches" | xargs -n 1 find .git/refs/heads -name | xargs basename -a`
+	local real_branches=`echo "$eligible_branches" | xargs -n 1 find .git/refs/heads -name | xargs -n 1 basename`
 
 	# if version is a branch, picks it
 	local branch=`echo "$real_branches" | grep "^$1$" | head -n 1`
